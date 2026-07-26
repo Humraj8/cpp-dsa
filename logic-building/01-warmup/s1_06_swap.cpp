890a21bef4749d131b44f3714e8c2161fb555517
Humraj8 <humrajchetry7@gmail.com>
2026-07-26 16:23:48 +0530

    learned swapping 2 numbers here using a third variable also learnt about core meaning of assignment operator ' = '

---
+++ logic-building/01-warmup/s1_06_swap.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+// Write a program to swap two numbers using a third variable.
+int a=2;
+int b=3;
+int c;
+ c=a;
+ a=b;
+b=c;
+cout<<a<<endl;
+cout<<b;
+return 0;
+// = means??
+//take whatever it is on the right side and shove it in the left side
+
+
+}
