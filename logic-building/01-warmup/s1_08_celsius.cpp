e4f743359650ff739d5092039055ee427d8e0f3c
Humraj8 <humrajchetry7@gmail.com>
2026-07-26 20:43:07 +0530

    celsius to farhenheit conversion

---
+++ logic-building/01-warmup/s1_08_celsius.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+//Write a program to read a temperature in Celsius and convert it to Fahrenheit.
+float c;
+cin>>c;
+cout<<"the temprature in farhenheit is :"<<((c*9/5)+32);
+return 0;
+}
