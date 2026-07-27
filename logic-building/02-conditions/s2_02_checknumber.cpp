96f2ad4db3dbeb705727b8bfb5be2dfd546f53b0
Humraj8 <humrajchetry7@gmail.com>
2026-07-27 19:49:28 +0530

    Solve positive, negative, or zero conditional problem

---
+++ logic-building/02-conditions/s2_02_checknumber.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+//Write a program to read a number and check whether it is positive, negative or zero.
+
+float num;
+cin>>num;
+if(num>0){
+    cout<<"the number is positive";
+
+}
+else if(num<0){
+
+    cout<<"the number is negative";
+}
+else{
+
+    cout<<"the number is zero";
+}
+
+
+
+    return 0;
+}
