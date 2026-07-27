fded11747ce06326cec37923deeac84db86871bd
Humraj8 <humrajchetry7@gmail.com>
2026-07-27 20:09:07 +0530

    find largest number amoung 3 number

---
+++ logic-building/02-conditions/tempCodeRunnerFile.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+//⁠Write a program to read three numbers and find the largest among them.
+float num1,num2,num3;
+cin>>num1>>num2>>num3;
+if (num1>num2 && num1>num3){
+    cout<<"num1 is greatest";
+
+}
+else if(num2>num1 && num2>num3){
+
+cout<<"num2 is greatest ";
+
+}
+else{
+
+    cout<<"num3 is greatest";
+}
+
+
+
+    return 0;
+}
