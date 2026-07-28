e1fda2d039b12cce2f9e818b2a8d869b6956ad5b
Humraj8 <humrajchetry7@gmail.com>
2026-07-28 16:20:24 +0530

    Refactor problem 16 about vowel and learnt to use tolower()

--- logic-building/02-conditions/s2_06_vowel.cpp
+++ logic-building/02-conditions/s2_06_vowel.cpp
@@ -2,10 +2,14 @@
 using namespace std;
 int main(){
 //⁠Write a program to read a character and check whether it is a vowel or a consonant.
-char ch;
-cin>>ch;
+char ch; // User inputs 'A'
+
+cin>>ch; // 'A' gets turned into 'a', and stored back into 'ch'
+
 ch= tolower(ch);
+
 if (ch == 'a'||ch == 'e' ||ch == 'i' ||ch == 'o'|| ch == 'u'){
+    // Since ch is now 'a', it matches 'a' here and prints Vowel!
     cout<<"the character is vowel";
 
 }
@@ -13,13 +17,10 @@ else
 {
     cout<<"the character is consonant";
 }
-
-
+return 0;
+}
 /*
 ch= tolower(ch); means in ch what ever we gave upper  if it is upper(A) it change the character to lowercase(a) as u can see the below lower case character(a) matches with coming lowercase(a) ,so now it prints chatacter is vowel ...
 with the help of this we dont have to write 10 conditions
 if (ch == 'a'||ch == 'e' ||ch == 'i' ||ch == 'o'|| ch == 'u'||ch == 'A'||ch == 'E' ||ch == 'I' ||ch == 'O'|| ch == 'U')
 */
-
-    return 0;
-}
