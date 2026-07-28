5683e6c9fdca12b3b69cfb773dddbb41b18faceb
Humraj8 <humrajchetry7@gmail.com>
2026-07-28 18:29:17 +0530

    voting eligibility check ,finished phase 2

---
+++ logic-building/02-conditions/s2_10_vote.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+//Write a program to read the age of a person and check whether they are eligible to vote.
+int age;
+cin>>age;
+if(age>=18){
+cout<<"the individual is allowed to vote";
+}
+else {
+    cout<<"the individual is not allowed to vote";
+}
+
+}
