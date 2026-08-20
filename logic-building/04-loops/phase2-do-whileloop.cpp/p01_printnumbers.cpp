9357182878081563e2f871be7e757211902dc813
Humraj8 <humrajchetry7@gmail.com>
2026-08-20 21:13:00 +0530

    feat(do-whileloop):prints numbers from 1 to 10 using do-while loops

---
+++ logic-building/04-loops/phase2-do-whileloop.cpp/p01_printnumbers.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+//Print numbers from 1 to 10.
+
+int i=1;
+
+do
+{
+   cout<<i<<endl;
+   i++; 
+} while (i<=10);
+
+//the syntax is it prints first no matter what and after print first it checks it is ture or not ..if it is not true then it exits other than that it goes on loop
+
+    return 0;
+}
