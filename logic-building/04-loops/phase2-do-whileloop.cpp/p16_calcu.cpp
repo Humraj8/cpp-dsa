4c35a99236412f2f22b5b5aacb3d737216d46df5
Humraj8 <humrajchetry7@gmail.com>
2026-08-30 22:02:43 +0530

    feat(do-while):print operations by using user driven menu

---
+++ logic-building/04-loops/phase2-do-whileloop.cpp/p16_calcu.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+//Implement a simple calculator using a menu.
+int n;
+int a,b;
+do {
+cout<<"1. additon"<<endl;
+cout<<"2. subtraction"<<endl;
+cout<<"3. multiplication"<<endl;
+cout<<"4. division"<<endl;
+cout<<"5. exit"<<endl;
+cin>>n;
+
+if(n>=1 && n<=4){
+  cout<<"enter numbers";
+  cin>>a>>b;
+}
+switch (n)
+{
+case 1:
+    cout<<"add.  :"<<(a+b)<<endl;
+    break;
+case 2:
+    cout<<"sub.   :"<<(a-b)<<endl;
+    break;
+case 3:
+    cout<<"multiply.  :"<<(a*b)<<endl;
+    break;
+case 4:
+    cout<<"div  :"<<(a/b)<<endl;
+    break;
+case 5:
+    cout<<"Exiting"<<endl;
+    break;
+default:
+ cout<<"invalid choice"<<endl;
+    break;
+}
+
+
+}while(n!=5);
+
+
+
+    return 0;
+}
