c3882dad7dddf8db52570d6de63dda45c2d70980
Humraj8 <humrajchetry7@gmail.com>
2026-09-01 18:45:59 +0530

    feat(patterns):print square patterns using nested forloops

---
+++ logic-building/04-loops/phase3-forloop/star-patterns(striver)/p1_squarepatterns.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+for (int i = 0; i <4; i++)
+{
+    for (int j = 0; j <4; j++)
+    {
+        cout<<"*";
+    }
+    cout<<endl;
+}
+
+/*  here the outer and inner loop i have figured out but
+i kept struggle to use endl inside the inner loop ,
+so we all use the endl outside the inner loop so as it completes that iteration 
+it goes to endl and goes to another line to make the square
+*/
+
+
+
+
+    return 0;
+}
