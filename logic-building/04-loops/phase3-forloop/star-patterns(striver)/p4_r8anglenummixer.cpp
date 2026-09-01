c288e27b9870e85bf8d3a081235ff6acfaaab82a
Humraj8 <humrajchetry7@gmail.com>
2026-09-01 21:15:18 +0530

    feat(patterns): print right trianle with number mixer using for nested loops

---
+++ logic-building/04-loops/phase3-forloop/star-patterns(striver)/p4_r8anglenummixer.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+void r8anglenummixer(int n){
+    for (int i = 1; i <= n; i++)
+{
+    for (int j = 1; j <= i; j++)
+    {
+        cout<<i<<" ";
+    }
+    cout<<endl;
+}
+}
+int main(){
+int testc;
+cin>>testc;
+ for (int i = 0; i < testc; i++)
+ {
+    int n;
+    cin>>n;
+    r8anglenummixer(n);
+ }
+ 
+
+
+
+
+}
