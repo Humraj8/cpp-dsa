71da753894208c51c3fb05dd808238397166e119
Humraj8 <humrajchetry7@gmail.com>
2026-09-01 19:06:52 +0530

    feat(patterns):print rightangle patterns

---
+++ logic-building/04-loops/phase3-forloop/star-patterns(striver)/p2_r8angletrianlge.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void printr8angle(int n){
+
+    for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j <=i; j++)
+    {
+       cout<<"* ";
+    }
+    cout<<endl;
+}
+
+
+
+}
+int main(){
+int n;
+cin>>n;
+printr8angle(n);
+
+    return 0;
+}
