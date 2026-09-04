a42b0fd4c7a15ccb68a8325fe2f2fd7209322ec5
Humraj8 <humrajchetry7@gmail.com>
2026-09-04 09:05:10 +0530

    feat(patterns):add pattern number 10

--- logic-building/04-loops/phase3-forloop/star-patterns(striver)/p7_allinonepatterns.cpp
+++ logic-building/04-loops/phase3-forloop/star-patterns(striver)/p7_allinonepatterns.cpp
@@ -95,6 +95,29 @@ for (int i = 0; i < n; i++)
     
 
 }
+void print10(int n){
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j <= i; j++)
+    {
+        cout<<"*";
+    }
+    cout<<endl;
+}
+
+for (int i = 0; i < n-1; i++)
+{
+    for (int j = 0; j < ((n-1)-i); j++)
+    {
+        cout<<"*";
+    }
+    cout<<endl;
+}
+
+
+
+
+}
 int main(){
 int testc;
 cin>>testc;
@@ -102,7 +125,7 @@ for (int i = 0; i < testc; i++)
 {
 int n;
 cin>>n;
-print9(n);
+print10(n);
 
 }
 
