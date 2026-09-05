88ec7c2ab0a27bfd99dc6c101680a3b0b15c8893
Humraj8 <humrajchetry7@gmail.com>
2026-09-05 19:11:43 +0530

    feat(patterns):add pattern number 11

--- logic-building/04-loops/phase3-forloop/star-patterns(striver)/p7_allinonepatterns.cpp
+++ logic-building/04-loops/phase3-forloop/star-patterns(striver)/p7_allinonepatterns.cpp
@@ -1,133 +1,143 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-void print7(int n){
-    for (int i =0 ; i < n; i++)
+void print7(int n)
 {
-    //for space
-   for (int j =0; j<(n-i-1) ; j++)
-   {
-    cout<<" ";
-   }
-  
-
-//for star patterns
-for (int k = 0; k<(2*i+1) ; k++)
-  {
-    cout<<"*";
-   }
-//for space 
-for (int j = 0; j<(n-i-1) ; j++)
-   {
-    cout<<" ";
-   }
-   cout<<endl;
-}
-
-
-}
-void print8(int n){
     for (int i = 0; i < n; i++)
     {
-       for (int j = 0; j < i; j++)
-       {
-        cout<<" ";
-       }
+        // for space
+        for (int j = 0; j < (n - i - 1); j++)
+        {
+            cout << " ";
+        }
 
-       for (int k = 0; k < ((2*n) -1 - (2*i)); k++)
-       {
-        cout<<"*";
-       }
+        // for star patterns
+        for (int k = 0; k < (2 * i + 1); k++)
+        {
+            cout << "*";
+        }
+        // for space
+        for (int j = 0; j < (n - i - 1); j++)
+        {
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
+void print8(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            cout << " ";
+        }
+
+        for (int k = 0; k < ((2 * n) - 1 - (2 * i)); k++)
+        {
+            cout << "*";
+        }
 
         for (int j = 0; j < i; j++)
-       {
-        cout<<" ";
-       }
-       
-       cout<<endl;
+        {
+            cout << " ";
+        }
+
+        cout << endl;
     }
-    
-    
 }
-void print9(int n){
-//sidha triangle
- for (int i =0 ; i < n; i++)
+void print9(int n)
 {
-    //for space
-   for (int j =0; j<(n-i-1) ; j++)
-   {
-    cout<<" ";
-   }
-  
-
-//for star patterns
-for (int k = 0; k<(2*i+1) ; k++)
-  {
-    cout<<"*";
-   }
-//for space 
-for (int j = 0; j<(n-i-1) ; j++)
-   {
-    cout<<" ";
-   }
-   cout<<endl;
-}
-
-//opposite of triangle
-for (int i = 0; i < n; i++)
+    // sidha triangle
+    for (int i = 0; i < n; i++)
     {
-       for (int j = 0; j < i; j++)
-       {
-        cout<<" ";
-       }
+        // for space
+        for (int j = 0; j < (n - i - 1); j++)
+        {
+            cout << " ";
+        }
 
-       for (int k = 0; k < ((2*n) -1 - (2*i)); k++)
-       {
-        cout<<"*";
-       }
+        // for star patterns
+        for (int k = 0; k < (2 * i + 1); k++)
+        {
+            cout << "*";
+        }
+        // for space
+        for (int j = 0; j < (n - i - 1); j++)
+        {
+            cout << " ";
+        }
+        cout << endl;
+    }
+
+    // opposite of triangle
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            cout << " ";
+        }
+
+        for (int k = 0; k < ((2 * n) - 1 - (2 * i)); k++)
+        {
+            cout << "*";
+        }
 
         for (int j = 0; j < i; j++)
-       {
-        cout<<" ";
-       }
-       
-       cout<<endl;
-    }
-    
+        {
+            cout << " ";
+        }
 
+        cout << endl;
+    }
 }
-void print10(int n){
-for (int i = 0; i < n; i++)
+void print10(int n)
 {
-    for (int j = 0; j <= i; j++)
+    for (int i = 0; i < n; i++)
     {
-        cout<<"*";
+        for (int j = 0; j <= i; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
     }
-    cout<<endl;
-}
 
-for (int i = 0; i < n-1; i++)
-{
-    for (int j = 0; j < ((n-1)-i); j++)
+    for (int i = 0; i < n - 1; i++)
     {
-        cout<<"*";
+        for (int j = 0; j < ((n - 1) - i); j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
     }
-    cout<<endl;
 }
-
-
-
-
-}
-int main(){
-int testc;
-cin>>testc;
-for (int i = 0; i < testc; i++)
+void print11(int n)
 {
-int n;
-cin>>n;
-print10(n);
+    for (int i = 0; i < n; i++)
+    {
+        int start = 1;
+        if (i % 2 == 0)
+            start = 1;
+        else
+            start = 0;
+        for (int j = 0; j <= i; j++)
+        {
+            cout << start;
+            start = 1 - start;
+        }
 
+        cout << endl;
+    }
 }
+int main()
+{
+    int testc;
+    cin >> testc;
+    for (int i = 0; i < testc; i++)
+    {
+        int n;
+        cin >> n;
+        print11(n);
+    }
 
     return 0;
 }
