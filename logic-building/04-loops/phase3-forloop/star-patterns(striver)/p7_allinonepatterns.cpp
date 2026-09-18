47af2f97e64ff7dc4a27508c7716ee9c5561b225
Humraj8 <humrajchetry7@gmail.com>
2026-09-18 20:56:25 +0530

    feat(pattern):add pattern number 22

--- logic-building/04-loops/phase3-forloop/star-patterns(striver)/p7_allinonepatterns.cpp
+++ logic-building/04-loops/phase3-forloop/star-patterns(striver)/p7_allinonepatterns.cpp
@@ -383,6 +383,22 @@ void print21(int n)
         cout << endl;
     }
 }
+void print22(int n)
+{
+    for (int i = 0; i < 2 * n - 1; i++)
+    {
+        for (int j = 0; j < 2 * n - 1; j++)
+        {
+            int top = i;
+            int left = j;
+            int right = (2 * n - 2) - j;
+            int down = (2 * n - 2) - j;
+            cout << (n - min(min(top, down), min(left, right))) << " ";
+        }
+        cout << endl;
+    }
+}
+
 int main()
 {
     int testc;
@@ -391,7 +407,7 @@ int main()
     {
         int n;
         cin >> n;
-        print20(n);
+        print22(n);
     }
 
     return 0;
